2777bb7bcfcdd7f2e4dd99f45d6b261bc1aa75aa
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2025-12-21 01:31:50 +0530

    Implemented, getLaneCentre, road- complete

--- road.js
+++ road.js
@@ -3,32 +3,48 @@ class Road {
         this.x = x;
         this.width = width;
         this.laneCount = laneCount;
+
         this.left = x - width / 2;
         this.right = x + width / 2;
+
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+        const topLeft = (this.left, this.top);
+        const topRight = (this.right, this.top);
+        const bottomLeft = (this.left, this.bottom);
+        const bottomRight = (this.right, this.bottom);
+        this.borders = [[topLeft, bottomLeft], [topRight, bottomRight]];
+    }
+
+    getLaneCenter(laneIndex){
+        const laneWidth = this.width/this.laneCount;
+        return this.left + laneWidth/2 + Math.min(laneIndex, this.laneCount-1)*laneWidth;
     }
 
     draw(ctx) {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        for (let i = 0; i <= this.laneCount; i++) {
+        for (let i = 1; i <= this.laneCount-1; i++) {
             const x = lerp(
                 this.left,
                 this.right,
                 i / this.laneCount
             );
-            if(i>0 && i<this.laneCount){
-                ctx.setLineDash([20, 20]);
-            }else{
-                ctx.setLineDash([]);
-            }
+            ctx.setLineDash([20, 20]);
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        });
     }
 }
