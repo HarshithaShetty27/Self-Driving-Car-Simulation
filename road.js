39d35bc74e67bbeade7bd665d2b63f97aa0451a0
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2025-12-23 01:00:51 +0530

    Artificial sensors done

--- road.js
+++ road.js
@@ -11,23 +11,23 @@ class Road {
         this.top = -infinity;
         this.bottom = infinity;
 
-        const topLeft = (this.left, this.top);
-        const topRight = (this.right, this.top);
-        const bottomLeft = (this.left, this.bottom);
-        const bottomRight = (this.right, this.bottom);
+        const topLeft = { x: this.left, y: this.top };
+        const topRight = { x: this.right, y: this.top };
+        const bottomLeft = { x: this.left, y: this.bottom };
+        const bottomRight = { x: this.right, y: this.bottom };
         this.borders = [[topLeft, bottomLeft], [topRight, bottomRight]];
     }
 
-    getLaneCenter(laneIndex){
-        const laneWidth = this.width/this.laneCount;
-        return this.left + laneWidth/2 + Math.min(laneIndex, this.laneCount-1)*laneWidth;
+    getLaneCenter(laneIndex) {
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneWidth / 2 + Math.min(laneIndex, this.laneCount - 1) * laneWidth;
     }
 
     draw(ctx) {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        for (let i = 1; i <= this.laneCount-1; i++) {
+        for (let i = 1; i <= this.laneCount - 1; i++) {
             const x = lerp(
                 this.left,
                 this.right,
@@ -40,7 +40,7 @@ class Road {
             ctx.stroke();
         }
         ctx.setLineDash([]);
-        this.borders.forEach(border=>{
+        this.borders.forEach(border => {
             ctx.beginPath();
             ctx.moveTo(border[0].x, border[0].y);
             ctx.lineTo(border[1].x, border[1].y);
